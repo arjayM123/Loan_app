e47a9b0b7b194575100746d7f279168a2c685a00
arjayM123 <arjaymabini123@gmail.com>
2026-01-30 02:12:47 +0800

    update

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-require_once '/Loan-system/config.php';
+require_once 'Loan-system/config.php';
 
 if (isLoggedIn()) {
     if (isAdmin()) {
-        redirect('admin_dashboard.php');
+        redirect('admin_pages/admin_dashboard.php');
     } else {
-        redirect('user_dashboard.php');
+        redirect('User_pages/user_dashboard.php');
     }
 } else {
     redirect('Loan-system/login.php');
